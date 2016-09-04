eb1e40c7096eed5d38b95e52aeba1094734c0416
全宏林 <qhl0645@175game.com>
2016-09-04 07:32:42 -0800

    #删除urls.py未用到的url

--- mblog/mblog/urls.py
+++ mblog/mblog/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from filebrowser.sites import site
+
 
 admin.autodiscover()
 urlpatterns = [
-    url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^grappelli/',include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth/', include('mblog.oauth.urls')),
